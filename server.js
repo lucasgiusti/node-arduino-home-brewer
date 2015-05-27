bc151d1fba652c9f60d6ca86b68fc94fa9e9e962
lucas.giusti <giusti.lucas@gmail.com>
2015-05-27 10:01:35 -0300

    first commit server.js

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ var application_root = __dirname,
     LocalStrategy = require('passport-local').Strategy,
     iz = require('iz'),
     ObjectID = require('mongodb').ObjectID,
-    passportLocalMongoose = require('passport-local-mongoose'),
-
-    accountRoute = require("./routes/account"),
-    userRoute = require("./routes/user"),
-    studentRoute = require("./routes/student"),
-    managerRoute = require("./routes/manager"),
-    utilRoute = require("./routes/util");
+    passportLocalMongoose = require('passport-local-mongoose');
 
 
 // Config
@@ -48,59 +42,7 @@ var application_root = __dirname,
 
 
 
-var AccountModel = accountRoute.AccountModel;
-var UserModel = userRoute.UserModel;
-var auth = accountRoute.auth;
-var isAuthorized = accountRoute.isAuthorized;
-
-
-
-
-//************************************************************
-var connectionString = 'mongodb://localhost:27017/MongoRestFullCRUD';
-mongoose.connect(connectionString);
-
-accountRoute.CreateAdmUser();
-
-
-
-
-//************************************************************
-// ACCOUNT
-app.get('/account/:username', auth, accountRoute.findByUserName);
-app.put('/account/:id', auth, accountRoute.putAccount);
-app.get('/loggedtest', accountRoute.loggedtest);
-app.post('/login', accountRoute.login);
-app.get('/logout', accountRoute.logout);
-
-// STUDENTS
-app.get('/students', auth, studentRoute.getStudentsAll);
-app.get('/students/name/:name', auth, studentRoute.getStudentsByName);
-app.get('/students/cpf/:cpf', auth, studentRoute.getStudentsByCpf);
-app.get('/students/registration/:registration', auth, studentRoute.getStudentsByRegistration);
-app.get('/students/:id', auth, studentRoute.getStudentsById);
-app.get('/studentsactive', auth, studentRoute.getStudentsActive);
-app.put('/students/:id', auth, studentRoute.putStudent);
-app.delete('/students/:id', auth, studentRoute.delStudent);
-app.post('/students', auth, studentRoute.postStudent);
-
-// MANAGERS
-app.get('/managers', auth, managerRoute.getManagersAll);
-app.get('/managers/name/:name', auth, managerRoute.getManagersByName);
-app.get('/managers/cpf/:cpf', auth, managerRoute.getManagersByCpf);
-app.get('/managers/registration/:registration', auth, managerRoute.getManagersByRegistration);
-app.get('/managers/:id', auth, managerRoute.getManagersById);
-app.put('/managers/:id', auth, managerRoute.putManager);
-app.delete('/managers/:id', auth, managerRoute.delManager);
-app.post('/managers', auth, managerRoute.postManager);
-//************************************************************
-
-// RELATORIOS
-app.get('/relusers', auth, userRoute.getRelUsersAll);
-app.get('/xmlcompleteusers', auth, userRoute.getXmlCompleteUsersAll);
-
-
 // Launch server
 http.createServer(app).listen(app.get('port'), function () {
-    console.log("Node Clinica Application server listening on port " + app.get('port'));
+    console.log("Node Home Brewer Application server listening on port " + app.get('port'));
 });
