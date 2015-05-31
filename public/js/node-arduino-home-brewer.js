c9f0f97a664060337b435655f71074c08ec4b57b
lucasgiusti <giusti.lucas@gmail.com>
2015-05-31 20:11:30 -0300

    ajustando layout

--- public/js/node-arduino-home-brewer.js
+++ public/js/node-arduino-home-brewer.js
@@ -1,14 +1,14 @@
 ﻿
 function defineLayout() {
     $(".btn-medio").css("width", "95px");
-    $(".btn-grande").css("width", "195px");
+   // $(".btn-grande").css("width", "195px");
     $(".diminuir").css("width", "40px");
     $(".aumentar").css("width", "40px");
     $(".txt-valor").css("width", "105px");
     $(".lbl-titulo").css("width", "195px");
 
     $(".btn-medio").css("margin-bottom", "5px");
-    $(".btn-grande").css("margin-bottom", "5px");
+//    $(".btn-grande").css("margin-bottom", "5px");
     $(".btn-com-legenda").css("margin-right", "1px");
     $(".voltar-1-botao").css("margin-top", "156px");
     $(".voltar-1-botao-2-legendas").css("margin-top", "106px");
@@ -216,4 +216,4 @@ $(".log").click(function () {
     swal({ title: "", text: texto, confirmButtonColor: '#428bca' });
 });
 
-//defineLayout();
+defineLayout();
