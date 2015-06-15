548a8f7f44371d900b1f07cc730ce5425cd65053
lucasgiusti <giusti.lucas@gmail.com>
2015-06-14 23:17:12 -0300

    teste azure

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-﻿    //************************************************************
+﻿﻿//************************************************************
 
 var application_root = __dirname,
     express = require("express"),
-    session = require("express-session"),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
     path = require("path"),
+    http = require('http'),
     fs = require('fs'),
     mongoose = require('mongoose'),
     app = express(),
-    http = require('http').Server(app),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     iz = require('iz'),
     ObjectID = require('mongodb').ObjectID,
     passportLocalMongoose = require('passport-local-mongoose'),
-
     accountRoute = require("./routes/account");
+
+
 // Config
+    app.configure(function () {
         app.set('port', process.env.PORT || 3000);
-        app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(bodyParser.json());
-        app.use(session({ secret: 'nodehomebrewerkey', resave: true, saveUninitialized: true }));
+        app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+        app.use(express.bodyParser());
         app.use(express.static(path.join(__dirname, 'public')));
-        app.use(cookieParser());
+        app.use(express.cookieParser('your secret here'));
+        app.use(express.session());
         app.use(function (req, res, next) {
             if (req.method == 'POST' && req.url == '/login') {
 
@@ -38,29 +37,34 @@ var application_root = __dirname,
         });
         app.use(passport.initialize());
         app.use(passport.session());
+    });
 //************************************************************
 
 
-        var AccountModel = accountRoute.AccountModel;
-        var auth = accountRoute.auth;
-        var isAuthorized = accountRoute.isAuthorized;
 
-
-        //************************************************************
-        var connectionString = require('./models/conn');
-        mongoose.connect(connectionString);
-
-        accountRoute.CreateAdmUser();
+var AccountModel = accountRoute.AccountModel;
 
 
 
 
-        //************************************************************
-        // ACCOUNT
-        app.get('/loggedtest', accountRoute.loggedtest);
+//************************************************************
+var connectionString = require('./models/conn');
+mongoose.connect(connectionString);
+
+accountRoute.CreateAdmUser();
+
+
+
+
+//************************************************************
+// ACCOUNT
+app.get('/loggedtest', accountRoute.loggedtest);
+app.post('/login', accountRoute.login);
+app.get('/logout', accountRoute.logout);
+
 
 
 // Launch server
-        http.listen(3000, function () {
-            console.log('listening on *:3000');
-        });
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Node Application server listening on port " + app.get('port'));
+});
